0b705d1a2b2ceb5ddd3e0e7917629cbb29c16abc
Nikith13 <mogallapallinagavenkateswara.mogallapalli@anko.com>
2025-01-07 11:51:18 +0530

    update

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
  module.exports = {
-    branches: ['mmogalla/pull_request_file'],
+    branches: ['main'],
     tagFormat: 'v${version}',
     plugins: [
       '@semantic-release/commit-analyzer',
